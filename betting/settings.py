bd5e9701b34a036d644a3b94cfa288723a8d5942
camazg <43435449+camazg@users.noreply.github.com>
2019-01-03 13:51:53 +0000

    Update settings.py

--- betting/settings.py
+++ betting/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w-5rxgmdrj4r(cm-4uir9hw@xsb%n(k_=0$sg@2myf=)-q0fi5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
